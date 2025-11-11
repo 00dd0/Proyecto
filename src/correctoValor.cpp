d9f381daa78074e317465f0fb8a8cfb3b65c0c53
00dd0 <damirandaae@gmail.com>
2025-11-10 23:56:09 -0500

    Create correctoValor.cpp

---
+++ src/correctoValor.cpp
@@ -0,0 +1,9 @@
+#include "correctoValor.h"
+#include <iostream>
+
+
+void compValor(){
+        std::cin.clear();
+		std::cin.ignore(10000, '\n');
+		std::cout << "Entrada inválida. Ingrese solo números.\n";
+}
