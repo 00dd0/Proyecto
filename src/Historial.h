39c8a37d0e98743e7d6ea793b298d89c92de6f5d
faridlopez132007-glitch <faridlopez132007@gmail.com>
2025-11-11 20:14:36 -0500

    Update Historial.h

--- src/Historial.h
+++ src/Historial.h
@@ -1,1 +1,9 @@
+#ifndef HISTORIAL.h
+#define HISTORIAL.h
 
+#include <string>
+
+void guardarHistorial(const std::string& ganador, int movimientos = 0, int duracionSeg = );
+void mostrarHistorial();
+
+#endif;
